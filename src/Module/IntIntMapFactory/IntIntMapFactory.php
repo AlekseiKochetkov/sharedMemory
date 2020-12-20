ba61585cb53e6c9863902ae94e00ef09bf0c7ba9
Aleksei Kochetkov <kochetkov.a@gmedia.tech>
2020-12-20 18:53:41 +0300

    factory to createe IntIntMap

--- src/Module/IntIntMapFactory/IntIntMapFactory.php
+++ src/Module/IntIntMapFactory/IntIntMapFactory.php
@@ -7,8 +7,16 @@ final class IntIntMapFactory implements IntIntMapFactoryInterface
 {
     public function create(int $size): IntIntMapInterface
     {
-      return new IntIntMap(
-
-      );
+        return new IntIntMap(
+            shmop_open($this->createKey(), "w", 0600, $size),
+            $size
+        );
     }
+
+    private function createKey(): int
+    {
+        return 0;
+    }
+
+
 }
